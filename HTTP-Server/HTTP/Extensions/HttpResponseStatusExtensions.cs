8ff08fecbbddbfe1156428ae828f95628c07a056
Kiril Pecev <kir4o_pecev@abv.bg>
2020-04-06 01:08:00 +0300

    added extension method

---
+++ HTTP-Server/HTTP/Extensions/HttpResponseStatusExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace HTTP.Extensions
+{
+    using HTTP.Enums;
+    using System.Text.RegularExpressions;
+
+    public static class HttpResponseStatusExtensions
+    {
+        private static Regex UpperCamelCaseRegex = new Regex(@"(?<!^)((?<!\d)\d|(?(?<=[A-Z])[A-Z](?=[a-z])|[A-Z]))", RegexOptions.Compiled);
+
+        /// <summary>
+        /// Get response status code value and name
+        /// </summary>
+        /// <returns></returns>
+        public static string GetResponseLine(this HttpResponseStatusCode code)
+        {
+            short codeValue = (short)code;
+            string codeName = codeValue == 200 ? "OK" : code.AsUpperCamelCaseName();
+
+            return $"{codeValue} {codeName}";
+        }
+
+        private static string AsUpperCamelCaseName(this HttpResponseStatusCode code)
+        {
+            return UpperCamelCaseRegex.Replace(code.ToString(), " $1").Capitalize();
+        }
+    }
+}
---
+++ HTTP-Server/HTTP/Extensions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace HTTP.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class StringExtensions
+    {
+        public static string Capitalize(this string str)
+        {
+            List<string> result = new List<string>();
+            string[] arr = str.Split(' ');
+
+            foreach (var s in arr)
+            {
+                result.Add(s[0].ToString().ToUpper() + s.Substring(1).ToLower());
+            }
+
+            return String.Join(" ", result);
+        }
+    }
+}
