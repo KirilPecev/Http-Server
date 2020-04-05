57875a805e5033009fd7f1d6d1e72c76ca15dc77
Kiril Pecev <kir4o_pecev@abv.bg>
2020-04-06 00:05:29 +0300

    added http exception

---
+++ HTTP-Server/HTTP/Exceptions/InternalServerErrorException.cs
@@ -0,0 +1,9 @@
+﻿namespace HTTP.Exceptions
+{
+    using System;
+
+    public class InternalServerErrorException : Exception
+    {
+        public override string Message => "The Server has encountered an error.";
+    }
+}
