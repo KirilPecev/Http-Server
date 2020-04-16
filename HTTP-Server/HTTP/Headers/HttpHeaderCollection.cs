e48c9653ece793246871fe4e8ba68cc17a3a9e60
Kiril Pecev <kir4o_pecev@abv.bg>
2020-04-16 22:35:00 +0300

    added http header collection

---
+++ HTTP-Server/HTTP/Headers/HttpHeaderCollection.cs
@@ -0,0 +1,32 @@
+﻿namespace HTTP.Headers
+{
+    using System.Collections.Generic;
+
+    public class HttpHeaderCollection : IHttpHeaderCollection
+    {
+        private readonly Dictionary<string, HttpHeader> headers;
+
+        public HttpHeaderCollection()
+        {
+            this.headers = new Dictionary<string, HttpHeader>();
+        }
+
+        public void Add(HttpHeader header)
+        {
+            if (!this.ContainsHeader(header.Key))
+            {
+                this.headers.Add(header.Key, header);
+            }
+        }
+
+        public bool ContainsHeader(string key)
+        {
+            return this.headers.ContainsKey(key);
+        }
+
+        public HttpHeader GetHeader(string key)
+        {
+            return this.headers[key];
+        }
+    }
+}
