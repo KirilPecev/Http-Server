5749a954ad9ca31e6b6e8c2380dd9c44fe1296ae
Kiril Pecev <kir4o_pecev@abv.bg>
2020-04-17 13:53:16 +0300

    added http header collection tests

---
+++ HTTP-Server/HTTP.Tests/HttpHeaderCollectionTests.cs
@@ -0,0 +1,36 @@
+﻿namespace HTTP.Tests
+{
+    using Headers;
+    using Xunit;
+
+    public class HttpHeaderCollectionTests
+    {
+        private readonly HttpHeaderCollection collection;
+        private readonly HttpHeader header;
+
+
+        public HttpHeaderCollectionTests()
+        {
+            this.collection = new HttpHeaderCollection();
+            this.header = this.header = new HttpHeader("content-type", "application/json");
+        }
+
+        [Fact]
+        public void Add_ShouldAddHeader()
+        {
+            string errorMessage = "HttpHeaderCollection Add() method does not add header!";
+            this.collection.Add(this.header);
+
+            Assert.True(this.collection.ContainsHeader("content-type"), errorMessage);
+        }
+
+        [Fact]
+        public void GetHeader_ShouldGetHeader()
+        {
+            this.collection.Add(this.header);
+            HttpHeader result = this.collection.GetHeader("content-type");
+            
+            Assert.NotNull(result);
+        }
+    }
+}
