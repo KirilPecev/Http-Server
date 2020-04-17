36ce63578fb7310ebe2f6c574b084950b3d6a9ca
Kiril Pecev <kir4o_pecev@abv.bg>
2020-04-17 13:23:22 +0300

    added tests for string extensions class

---
+++ HTTP-Server/HTTP.Tests/StringExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿namespace HTTP.Tests
+{
+    using Extensions;
+    using Xunit;
+
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void Capitalize_ShouldCapitalizeString()
+        {
+            var result = "hello".Capitalize();
+
+            Assert.Equal("Hello", result);
+        }
+
+        [Fact]
+        public void Capitalize_ShouldCapitalizeStrings()
+        {
+            var result = "Hello World".Capitalize();
+
+            Assert.Equal("Hello World", result);
+        }
+    }
+}
