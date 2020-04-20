2fd07e03ec6222d904583ac811bff2d3b771447a
Kiril Pecev <kir4o_pecev@abv.bg>
2020-04-20 13:01:07 +0300

    added web server results

---
+++ HTTP-Server/WebServer/Results/TextResult.cs
@@ -0,0 +1,16 @@
+﻿namespace WebServer.Results
+{
+    using HTTP.Enums;
+    using HTTP.Headers;
+    using HTTP.Responses;
+    using System.Text;
+
+    public class TextResult : HttpResponse
+    {
+        public TextResult(string content, HttpResponseStatusCode statusCode) : base(statusCode)
+        {
+            this.Headers.Add(new HttpHeader("Content-Type", "text/plain"));
+            this.Content = Encoding.UTF8.GetBytes(content);
+        }
+    }
+}
