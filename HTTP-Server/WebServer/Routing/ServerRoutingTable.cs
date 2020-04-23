70eb7e1a830470689e450fc83694caa1950b38a2
Kiril Pecev <kir4o_pecev@abv.bg>
2020-04-23 21:32:16 +0300

    added routing

---
+++ HTTP-Server/WebServer/Routing/ServerRoutingTable.cs
@@ -0,0 +1,24 @@
+﻿namespace WebServer.Routing
+{
+    using HTTP.Enums;
+    using HTTP.Requests;
+    using HTTP.Responses;
+    using System;
+    using System.Collections.Generic;
+
+    public class ServerRoutingTable
+    {
+        public ServerRoutingTable()
+        {
+            this.Routes = new Dictionary<HttpRequestMethod, IDictionary<string, Func<IHttpRequest, IHttpResponse>>>()
+            {
+                [HttpRequestMethod.Get] = new Dictionary<string, Func<IHttpRequest, IHttpResponse>>(),
+                [HttpRequestMethod.Post] = new Dictionary<string, Func<IHttpRequest, IHttpResponse>>(),
+                [HttpRequestMethod.Put] = new Dictionary<string, Func<IHttpRequest, IHttpResponse>>(),
+                [HttpRequestMethod.Delete] = new Dictionary<string, Func<IHttpRequest, IHttpResponse>>()
+            };
+        }
+
+        public IDictionary<HttpRequestMethod, IDictionary<string, Func<IHttpRequest, IHttpResponse>>> Routes { get; set; }
+    }
+}
