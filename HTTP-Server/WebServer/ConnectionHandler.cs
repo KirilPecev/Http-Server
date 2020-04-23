ed6cbc06a0228ab4862e1fe704f48d9230f731a6
Kiril Pecev <kir4o_pecev@abv.bg>
2020-04-23 21:49:11 +0300

    added server class

---
+++ HTTP-Server/WebServer/ConnectionHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace WebServer
+{
+    using System;
+    using System.Net.Sockets;
+    using System.Threading.Tasks;
+    using Routing;
+
+    public class ConnectionHandler
+    {
+        public ConnectionHandler(Socket client, ServerRoutingTable routingTable)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task ProcessRequestAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HTTP-Server/WebServer/Server.cs
@@ -0,0 +1,51 @@
+﻿namespace WebServer
+{
+    using Routing;
+    using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Threading.Tasks;
+
+    public class Server
+    {
+        private const string LocalhostIpAddress = "127.0.0.1";
+
+        private readonly int port;
+
+        private readonly TcpListener tcpListener;
+
+        private readonly ServerRoutingTable routingTable;
+
+        private bool isRunning;
+
+        public Server(int port, ServerRoutingTable routingTable)
+        {
+            this.port = port;
+            this.routingTable = routingTable;
+
+            this.tcpListener = new TcpListener(IPAddress.Parse(LocalhostIpAddress), port);
+        }
+
+        public void Run()
+        {
+            this.tcpListener.Start();
+            this.isRunning = true;
+
+            Console.WriteLine($"Server started at http://{LocalhostIpAddress}:{this.port}");
+
+            Task task = Task.Run(this.ListenLoop);
+            task.Wait();
+        }
+
+        public async Task ListenLoop()
+        {
+            while (this.isRunning)
+            {
+                Socket client = await this.tcpListener.AcceptSocketAsync();
+                ConnectionHandler connectionHandler = new ConnectionHandler(client, this.routingTable);
+                Task responseTask = connectionHandler.ProcessRequestAsync();
+                responseTask.Wait();
+            }
+        }
+    }
+}
