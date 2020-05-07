402d7828b8be6b232e0a27276e4b4d0b0ff27505
Kiril Pecev <kir4o_pecev@abv.bg>
2020-05-07 21:51:38 +0300

    added demo project

---
+++ HTTP-Server/Demo/Launcher.cs
@@ -0,0 +1,20 @@
+﻿namespace Demo
+{
+    using Controllers;
+    using HTTP.Enums;
+    using WebServer;
+    using WebServer.Routing;
+
+    public class Launcher
+    {
+        public static void Main(string[] args)
+        {
+            ServerRoutingTable routingTable = new ServerRoutingTable();
+            routingTable.Routes[HttpRequestMethod.Get]["/"] = request => new HomeController().Index();
+
+            Server server = new Server(8000, routingTable);
+
+            server.Run();
+        }
+    }
+}
