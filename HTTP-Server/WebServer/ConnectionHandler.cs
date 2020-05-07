4de06452795153c883076839ef487872dad1c4b5
Kiril Pecev <kir4o_pecev@abv.bg>
2020-05-07 21:51:14 +0300

    implemented ConnectionHandler class

--- HTTP-Server/WebServer/ConnectionHandler.cs
+++ HTTP-Server/WebServer/ConnectionHandler.cs
@@ -1,20 +1,89 @@
 ﻿namespace WebServer
 {
+    using HTTP.Enums;
+    using HTTP.Requests;
+    using HTTP.Responses;
+    using Routing;
     using System;
     using System.Net.Sockets;
+    using System.Text;
     using System.Threading.Tasks;
-    using Routing;
 
     public class ConnectionHandler
     {
+        private readonly Socket client;
+
+        private readonly ServerRoutingTable serverRoutingTable;
+
         public ConnectionHandler(Socket client, ServerRoutingTable routingTable)
         {
-            throw new NotImplementedException();
+            this.client = client;
+            this.serverRoutingTable = routingTable;
+        }
+
+
+        private async Task<IHttpRequest> ReadRequest()
+        {
+            StringBuilder result = new StringBuilder();
+            ArraySegment<byte> data = new ArraySegment<byte>(new byte[1024]);
+
+            while (true)
+            {
+                int numberOfBytesRead = await this.client.ReceiveAsync(data, SocketFlags.None);
+
+                if (numberOfBytesRead == 0)
+                {
+                    break;
+                }
+
+                string bytesAsString = Encoding.UTF8.GetString(data.Array, 0, numberOfBytesRead);
+                result.Append(bytesAsString);
+
+                if (numberOfBytesRead < 1023)
+                {
+                    break;
+                }
+            }
+
+            if (result.Length == 0)
+            {
+                return null;
+            }
+
+            return new HttpRequest(result.ToString());
+        }
+
+        private IHttpResponse HandleResponse(IHttpRequest httpRequest)
+        {
+            if (!this.serverRoutingTable.Routes.ContainsKey(httpRequest.RequestMethod)
+            || !this.serverRoutingTable.Routes[httpRequest.RequestMethod].ContainsKey(httpRequest.Path))
+            {
+                return new HttpResponse(HttpResponseStatusCode.NotFound);
+            }
+
+            return this.serverRoutingTable.Routes[httpRequest.RequestMethod][httpRequest.Path].Invoke(httpRequest);
+        }
+
+        private async Task PrepareResponse(IHttpResponse httpResponse)
+        {
+            byte[] bytes = httpResponse.GetBytes();
+            ArraySegment<byte> byteSegments = new ArraySegment<byte>(bytes);
+
+            await this.client.SendAsync(byteSegments, SocketFlags.None);
         }
 
         public async Task ProcessRequestAsync()
         {
-            throw new NotImplementedException();
+            IHttpRequest httpRequest = await this.ReadRequest();
+
+            if (httpRequest != null)
+            {
+                IHttpResponse httpResponse = this.HandleResponse(httpRequest);
+
+                await this.PrepareResponse(httpResponse);
+            }
+
+            this.client.Shutdown(SocketShutdown.Both);
         }
     }
 }
